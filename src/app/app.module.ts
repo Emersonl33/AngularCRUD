6c8ec71729feb1d9afc150d4141f3ac65d4b650f
emersonl33 <emersonl33@hotmail.com>
2025-01-24 21:11:00 -0300

    thoughts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,12 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
-
+    FooterComponent,
+    CreateThoughtComponent
   ],
   imports: [
     BrowserModule,
