836301573af20c78f83924083737d05fb1278d81
emersonl33 <emersonl33@hotmail.com>
2025-01-28 02:45:29 -0300

    Forms

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from './../thought';
 import { ThoughtService } from '../thought.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-thought',
@@ -20,19 +20,37 @@ export class CreateThoughtComponent {
   ){ }
     ngOnInit(): void {
       this.formulario = this.formBuilder.group({
-        conteudo: ['Olá, esse é o primeiro formulário reativo'],
-        autoria: ['EmersonGPT'],
+        conteudo: ['', Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+        autoria:
+          ['',
+            Validators.compose
+            ([Validators.required,
+              Validators.minLength(3)
+          ])],
         modelo: ['modelo1']
       })
     }
 
     criarPensamento() {
+      console.log(this.formulario.get('autoria')?.errors)
+      if(this.formulario.valid){
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento'])
       })
     }
+    }
 
     cancelCreate(){
       this.router.navigate(['/listarPensamento'])
+    }
+
+    habilitarBotao(): String{
+      if(this, this.formulario.valid) {
+        return 'botao'
+      } else{
+        return 'botao__desabilitado'
+      }
     }
 }
